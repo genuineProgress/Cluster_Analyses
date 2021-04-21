4a2b1975b37b4e352b21c10b4d48574f754a65da
a1 <artsemzdanovich@mail.ru>
2021-04-21 19:25:45 +0300

    Success. Next step is few remarks and dbscan algorithm.

--- Cluster_Search.cpp
+++ Cluster_Search.cpp
@@ -44,35 +44,7 @@ void Cluster_Search::create_edges_matrix () {
 }
 
 void Cluster_Search::wave_clustering () {
-    vector<bool> burnt (Point::quantity (), false);
-    // that cycle checks if point in marked cluster and if not creates new one
-    for (int m = 0; m < Point::quantity (); ++m) {
-        if (burnt[m]) {
-            continue;
-        }
-        vector<int> curr_wave = {m};
-        burnt[m] = true;
-        vector<int> next_wave; // points, connected to smth from curr_wave
-        vector<int> curr_cluster; // points from current cluster
-        // that cycle searches for neighbours of points in curr_wave
-        while (!curr_wave.empty ()) {
-            for (int i : curr_wave) {
-                for (int j = m + 1; j < Point::quantity (); ++j) {
-                    if (edges ()[i][j] && !burnt[j]) {
-                        next_wave.push_back (j);
-                        burnt[j]=true;
-                    }
-                }
-            }
-            curr_cluster.insert (curr_cluster.end (), curr_wave.begin (), curr_wave.end ());
-            curr_wave = next_wave;
-            for (auto &&i: next_wave) {
-                burnt[i] = true;
-            }
-            next_wave.clear ();
-        }
-        add (Cluster_Search::Cluster (curr_cluster));
-    }
+
 }
 
 vector<int> Cluster_Search::db_sorting (int density) {
--- Wave_algorithm.cpp
+++ Wave_algorithm.cpp
@@ -39,7 +39,36 @@ int Wave_algorithm::process(vector<Cluster_Search *> cluster_searches_, Field *f
         if (search_id >= cluster_searches_.size ()) {
             return -3;
         }
-        cluster_searches_[search_id]->wave();
+       // cluster_searches_[search_id]->wave();
+        vector<bool> burnt (Point::quantity (), false);
+      // that cycle checks if point in marked cluster and if not creates new one
+        for (int m = 0; m < Point::quantity (); ++m) {
+          if (burnt[m]) {
+              continue;
+          }
+          vector<int> curr_wave = {m};
+          burnt[m] = true;
+          vector<int> next_wave; // points, connected to smth from curr_wave
+          vector<int> curr_cluster; // points from current cluster
+          // that cycle searches for neighbours of points in curr_wave
+          while (!curr_wave.empty ()) {
+              for (int i : curr_wave) {
+                  for (int j = m + 1; j < Point::quantity (); ++j) {
+                      if (cluster_searches_[search_id]->edges ()[i][j] && !burnt[j]) {
+                          next_wave.push_back (j);
+                          burnt[j]=true;
+                      }
+                  }
+              }
+              curr_cluster.insert (curr_cluster.end (), curr_wave.begin (), curr_wave.end ());
+              curr_wave = next_wave;
+              for (auto &&i: next_wave) {
+                  burnt[i] = true;
+              }
+              next_wave.clear ();
+          }
+          cluster_searches_[search_id]->add (Cluster_Search::Cluster (curr_cluster));
+      }
         return 0;
 
     }
